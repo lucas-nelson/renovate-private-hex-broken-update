ed8814c2e9924a48d06332f55c6f8019dd0ece48
Lucas Nelson <lucas.nelson@3plearning.com>
2023-03-03 17:05:58 +1100

    Added a public package, and a private package
    The public package is a patch version behind and needs updating.

--- mix.exs
+++ mix.exs
@@ -23,6 +23,8 @@ defmodule RenovatePrivateHexBrokenUpdate.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.29.1"},
+      {:the_private_package, "~> 1.0", organization: "my_org"}
     ]
   end
 end
